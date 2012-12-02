f656fc727b3d66e90416ef11117b3846e9b3085e
Fernando Briano <fernando@picandocodigo.net>
2012-12-02 04:40:39 -0200

    Configs de los envs

--- api/config/boot.rb
+++ api/config/boot.rb
@@ -2,5 +2,8 @@ require 'rubygems'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+config = YAML.load_file('config/config.yml')
+ENV['TWITTER_CONSUMER_KEY'] ||= config['twitter']['consumer_key']
+ENV['TWITTER_CONSUMER_SECRET'] ||= config['twitter']['consumer_secret']
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201231829) do
+ActiveRecord::Schema.define(:version => 20121202031703) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -23,16 +23,16 @@ ActiveRecord::Schema.define(:version => 20121201231829) do
 
   create_table "issues", :force => true do |t|
     t.integer  "fixed"
-    t.integer  "relevance"
+    t.integer  "relevance",                 :default => 0
     t.string   "address"
     t.decimal  "longitude"
     t.decimal  "latitude"
     t.string   "image_url"
+    t.integer  "tweet_id",   :limit => 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "text"
     t.integer  "user_id"
-    t.string   "twitter_id"
   end
 
   create_table "issues_tags", :force => true do |t|
