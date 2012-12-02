d1272c85b072b417a94df7192de83fbf44f2132b
Fernando Briano <fernando@picandocodigo.net>
2012-12-01 22:30:49 -0200

    Agrega lógica de coordenadas

--- twitter/test.rb
+++ twitter/test.rb
@@ -1,11 +1,12 @@
 require 'tweetstream'
 require 'yaml'
 require 'json'
+require 'geocoder'
 
 config = YAML.load_file('config.yml')
 
 TweetStream.configure do |c|
-  c.consumer_key       = config['twitter']['consumer_key'] 
+  c.consumer_key       = config['twitter']['consumer_key']
   c.consumer_secret    = config['twitter']['consumer_secret']
   c.oauth_token        = config['twitter']['oauth_token']
   c.oauth_token_secret = config['twitter']['oauth_token_secret']
@@ -22,29 +23,14 @@ class Array
   end
 end
 
-TweetStream::Client.new.track(config['twitter']['hashtags']) do |status|
-
-  # require 'debugger'; debugger
-  if status.attrs[:retweeted_status] 
-    puts 'es un retweet'
-    retweet(status)
-  elsif status.attrs[:in_reply_to_status_id_str] 
-    puts 'es un reply'
-    reply(status)
-  else 
-    puts 'es uno nuevo'
-    new(status)
-  end
-
-end
 
 def retweet(t)
   url = config.api_host + '/twitter/rt'
   data = {
-    twitter_reply: status.attrs[:in_reply_to_status_id_str], 
-    message: status.attrs[:text], 
-    tweet_id: status.attrs[:id_str], 
-    user_id: status.attrs[:user][:in_reply_to_status_id_str], 
+    twitter_reply: status.attrs[:in_reply_to_status_id_str],
+    message: status.attrs[:text],
+    tweet_id: status.attrs[:id_str],
+    user_id: status.attrs[:user][:in_reply_to_status_id_str],
     image_url: status.attrs[:media][:media_url_https]
   }
 
@@ -54,10 +40,10 @@ end
 def reply(t)
   url = config.api_host + '/twitter/reply'
   data = {
-    message: status.attrs[:text], 
-    tweet_id: status.attrs[:id_str], 
+    message: status.attrs[:text],
+    tweet_id: status.attrs[:id_str],
     reply_to_id: status.attrs[:in_reply_to_status_id_str],
-    user_id: status.attrs[:user][:in_reply_to_status_id_str], 
+    user_id: status.attrs[:user][:in_reply_to_status_id_str],
     image_url: status.attrs[:media][:media_url_https],
   }
 
@@ -65,12 +51,29 @@ def reply(t)
 end
 
 def new(t)
+#status.attrs[:geo][:type]
+  if status.attrs[:geo]
+    case status.attrs[:geo][:type]
+      when "Point" then
+        latitude = status.attrs[:geo][:coordinates][0]
+        longitude = status.attrs[:geo][:coordinates][1]
+      when "Polygon" then
+        center = Geocoder::Calculations.geographic_center(
+                                                 status.attrs[:geo][:coordinates][0],
+                                                 status.attrs[:geo][:coordinates][1],
+                                                 status.attrs[:geo][:coordinates][2],
+                                                 status.attrs[:geo][:coordinates][3]
+                                                 )
+        latitude, longitude = center[0], center[1]
+    end
+  end
+
   url = config.api_host + '/twitter/new'
   data = {
     text: status.attrs[:text],
     image_url: status.attrs[:media][:media_url_https],
-    latitude: 0,
-    longitude: 0,
+    latitude: latitude,
+    longitude: longitude,
     twitter_id: status.attrs[:id_str],
     user_id: status.attrs[:user][:in_reply_to_status_id_str]
   }
@@ -81,8 +84,24 @@ end
 def post(json, uri)
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
-  request.body = json 
+  request.body = json
   response = http.request(request)
   response
 end
 
+
+TweetStream::Client.new.track(config['twitter']['hashtags']) do |status|
+
+  require 'debugger'; debugger
+  if status.attrs[:retweeted_status]
+    puts 'es un retweet'
+    retweet(status)
+  elsif status.attrs[:in_reply_to_status_id_str]
+    puts 'es un reply'
+    reply(status)
+  else
+    puts 'es uno nuevo'
+    new(status)
+  end
+
+end
