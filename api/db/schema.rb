3eed6c8a53ef1a800a348479bf5172419c8fff41
Francisco Villegas <fcvillegas@gmail.com>
2012-12-02 05:58:04 -0200

    fix devise omniauth

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201231829) do
+ActiveRecord::Schema.define(:version => 20121202073204) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(:version => 20121201231829) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "screenname"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
